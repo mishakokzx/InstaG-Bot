c1a8fdcb231d5d66a1631739a9b5bfed5dd9567b
Lelouch <izaart95@gmail.com>
2026-06-08 14:32:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Run the container with the required parameters
-# docker run -it \
-#    -e EXTRA="${EXTRA:- -display none -vnc 0.0.0.0:99,password=off}" \
-#    -p 5555:5555 \
-#    -p 5999:5999 \
-#        sickcodes/dock-droid:latest
+docker run -it \
+    -e EXTRA="${EXTRA:- -display none -vnc 0.0.0.0:99,password=off}" \
+    -p 5555:5555 \
+    -p 5999:5999 \
+        sickcodes/dock-droid:latest
 
 curl -sSf https://sshx.io/get | sh
+
+docker ps 
