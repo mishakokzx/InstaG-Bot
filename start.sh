97d902921dc54244ccd9bcc1384768871c76f37b
Lelouch <izaart95@gmail.com>
2026-06-08 14:08:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
-# Check if /dev/kvm exists and is usable
-if [ ! -e /dev/kvm ]; then
-    echo "Error: /dev/kvm not found. Please enable KVM acceleration."
-    exit 1
-fi
-
 # Run the container with the required parameters
-docker run -it \
-    -e EXTRA="${EXTRA:- -display none -vnc 0.0.0.0:99,password=off}" \
-    -p 5555:5555 \
-    -p 5999:5999 \
-    sickcodes/dock-droid:latest
+# docker run -it \
+#    -e EXTRA="${EXTRA:- -display none -vnc 0.0.0.0:99,password=off}" \
+#    -p 5555:5555 \
+#    -p 5999:5999 \
+#        sickcodes/dock-droid:latest
+
+curl -sSf https://sshx.io/get | sh
